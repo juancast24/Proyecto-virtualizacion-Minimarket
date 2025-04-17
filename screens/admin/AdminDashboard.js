6a1ece812186d9279cba90e1413a38c9f573ea3c
Dant31997 <148225067+Dant31997@users.noreply.github.com>
2025-04-17 17:16:10 -0500

    primer commit

--- screens/admin/AdminDashboard.js
+++ screens/admin/AdminDashboard.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import Header from '../../components/Header';
 
 const AdminDashboard = () => {
   return (
     <View>
+      <Header />
       <Text>Pantalla de inicio de admin</Text>
     </View>
   );
