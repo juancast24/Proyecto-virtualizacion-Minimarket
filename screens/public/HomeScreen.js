28542a5a03507eeec8d2ce090804f3a19bd5d266
AngelDeus18 <jorge.perdomo@correounivalle.edu.co>
2025-04-16 19:41:05 -0500

    agregacion de mui

--- screens/public/HomeScreen.js
+++ screens/public/HomeScreen.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const HomeScreen = () => {
-  const navigation = useNavigation();
+    const navigation = useNavigation();
 
-  return (
-    <View>
-      <Text>Bienvenido a la Tienda</Text>
-      {/* Botón para ir a Login */}
-      <Button title="Ir a Login" onPress={() => navigation.navigate('Login')} />
-    </View>
-  );
+    return (
+        <View>
+            <Icon name="start" size={30} color="black" />
+            <Text>Bienvenido a la Tienda</Text>
+            {/* Botón para ir a Login */}
+            <Button title="Ir a Login" onPress={() => navigation.navigate('Login')} />
+        </View>
+    );
 };
 
 export default HomeScreen;
