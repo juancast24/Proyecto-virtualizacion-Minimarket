01144dc3dac6b0d5d84471985478579b1fb4caf4
AngelDeus18 <jorge.perdomo@correounivalle.edu.co>
2025-04-16 23:28:49 -0500

    añadiendo useSafeAreaInsets

--- App.js
+++ App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { AuthProvider } from './context/AuthContext';
 import PublicNavigator from './navigation/PublicNavigator';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <AuthProvider>
-      <NavigationContainer>
-        <PublicNavigator />
-      </NavigationContainer>
-    </AuthProvider>
+    <SafeAreaProvider>
+      <AuthProvider>
+        <NavigationContainer>
+          <PublicNavigator />
+        </NavigationContainer>
+      </AuthProvider>
+    </SafeAreaProvider>
   );
 }
--- screens/public/HomeScreen.js
+++ screens/public/HomeScreen.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { View, Text, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const HomeScreen = () => {
     const navigation = useNavigation();
+    const insets= useSafeAreaInsets();
 
     return (
-        <View>
-            <Icon name="start" size={30} color="black" />
+        <View style={{paddingTop: insets.top, paddingBottom: insets.bottom}}>
+            <Icon name="home" size={30} color="black" />
             <Text>Bienvenido a la Tienda</Text>
             {/* Botón para ir a Login */}
             <Button title="Ir a Login" onPress={() => navigation.navigate('Login')} />
