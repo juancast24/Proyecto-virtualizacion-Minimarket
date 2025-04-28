99af09cc8f86f5b07bbd3096a9c1a3bcca8e5398
dilan.palta@correounivalle.edu.co <dilan.palta@correounivalle.edu.co>
2025-04-28 14:07:55 -0500

    avance login y correccion de bug de alertas

--- screens/public/LoginScreen.js
+++ screens/public/LoginScreen.js
@@ -1,86 +1,228 @@
-import React, { useState,useEffect } from 'react';
-import {
-  View,
-  Text,
-  TextInput,
-  Button,
-  StyleSheet,
-  Alert,
-  BackHandler,
-} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, StyleSheet, Alert, Image, TouchableOpacity} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { useAuth } from '../../context/AuthContext';
 
 const LoginScreen = () => {
+  const [isLogin, setIsLogin] = useState(true); // Determina si estamos en login o registro
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const { onLogin } = useAuth();
+  const [confirmPassword, setConfirmPassword] = useState(''); // Solo para registro
+  const [phone, setPhone] = useState(''); // Solo para registro
+
+  const { onLogin, onRegister } = useAuth();
   const navigation = useNavigation();
 
   const handleLogin = async () => {
-    console.log('▶ handleLogin fired', { username, password });
+    // Verificar si los campos están vacíos antes de llamar a onLogin
+    if (username.trim() === '' || password.trim() === '') {
+      return Alert.alert('Error', 'Hay campos sin rellenar');
+    }
+  
     const user = await onLogin(username.trim(), password);
-    if (!user || !password) {
+  
+    if (!user) {
       return Alert.alert('Error', 'Usuario o contraseña incorrectos');
     }
-
+  
+    // Redirigir dependiendo del rol del usuario
     if (user.role === 'admin') {
       navigation.navigate('AdminDashboard');
     } else {
       navigation.navigate('UserDashboard');
     }
   };
-
- // Sobrescribir el comportamiento del botón "Atrás"
- useEffect(() => {
-  const backHandler = BackHandler.addEventListener(
-    'hardwareBackPress',
-    () => {
-      navigation.navigate('Home'); // Redirige siempre al HomeScreen
-      return true; // Evita el comportamiento predeterminado
+  const handleRegister = async () => {
+    const success = await onRegister(username.trim(), password, confirmPassword.trim(), phone.trim());
+    if (success) {
+      setIsLogin(true); // Después de registro, volvemos al login
+      setUsername('');
+      setPassword('');
+      setConfirmPassword('');
+      setPhone('');
     }
-  );
-
-  return () => backHandler.remove(); // Limpia el evento al desmontar el componente
-}, [navigation]);
+  };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Login</Text>
+    <View style={styles.bg}>
+      <View style={styles.imageContainer}>
+        <Image source={require('../../assets/logo-market.png')} style={styles.logo} />
+      </View>
+      <View style={styles.container}>
 
-      {/* Inputs */}
-      <TextInput
-        placeholder="Usuario"
-        autoCapitalize="none"
-        value={username}
-        onChangeText={setUsername}
-        style={styles.input}
-      />
-      <TextInput
-        placeholder="Contraseña"
-        secureTextEntry
-        value={password}
-        onChangeText={setPassword}
-        style={styles.input}
-      />
+        {/* Botones de Login y Registro */}
+        <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={[
+            styles.button,
+            { backgroundColor: isLogin ? '#4CAF50' : '#ccc' }, // Color dinámico
+          ]}
+          onPress={() => setIsLogin(true)}
+        >
+          <Text style={styles.buttonText}>Iniciar Sesión</Text>
+        </TouchableOpacity>
 
-      {/* Botón real de Login */}
-      <Button title="Iniciar Sesión" onPress={handleLogin} />
-       <View style={styles.space} />
-      <Button title="Registrarse" onPress={() => navigation.navigate('RegisterScreen')}/>
+        <TouchableOpacity
+          style={[
+            styles.button,
+            { backgroundColor: !isLogin ? '#4CAF50' : '#ccc' }, // Color dinámico
+          ]}
+          onPress={() => setIsLogin(false)}
+        >
+          <Text style={styles.buttonText}>Registrarse</Text>
+        </TouchableOpacity>
+        </View>
+        
+        <View style={styles.imageContainer}>
+        <Image source={require('../../assets/persona.png')} style={styles.persona} />
+      </View>
 
-      
+          {/*    <Text style={styles.title}>{isLogin ? 'Iniciar Sesión' : 'Registro'}</Text> */}
+
+        {/* Formulario de Login */}
+        {isLogin ? (
+          <>
+            <TextInput
+              placeholder="Usuario"
+              autoCapitalize="none"
+              value={username}
+              onChangeText={setUsername}
+              style={styles.input}
+            />
+            <TextInput
+              placeholder="Contraseña"
+              secureTextEntry
+              value={password}
+              onChangeText={setPassword}
+              style={styles.input}
+            />
+            <TouchableOpacity style={styles.buttonLogin} onPress={handleLogin}>
+             <Text style={styles.buttonText}>Iniciar Sesión</Text>
+            </TouchableOpacity>
+          </>
+        ) : (
+          /* Formulario de Registro */
+          <>
+            <TextInput
+              placeholder="Usuario"
+              autoCapitalize="none"
+              value={username}
+              onChangeText={setUsername}
+              style={styles.input}
+            />
+            <TextInput
+              placeholder="Teléfono"
+              value={phone}
+              onChangeText={setPhone}
+              style={styles.input}
+              keyboardType="phone-pad"
+            />
+            <TextInput
+              placeholder="Contraseña"
+              secureTextEntry
+              value={password}
+              onChangeText={setPassword}
+              style={styles.input}
+            />
+            <TextInput
+              placeholder="Confirmar Contraseña"
+              secureTextEntry
+              value={confirmPassword}
+              onChangeText={setConfirmPassword}
+              style={styles.input}
+            />
+          <TouchableOpacity style={styles.buttonRegister} onPress={handleRegister}>
+            <Text style={styles.buttonText}>Registrarse</Text>
+          </TouchableOpacity>
+          </>
+        )}
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', padding: 20 },
-  title: { fontSize: 24, marginBottom: 20, textAlign: 'center' },
-  input: { borderWidth: 1, marginBottom: 15, padding: 10, borderRadius: 5 },
-  space: {
-    marginBottom: 20, // Ajusta el espacio entre los botones
+  bg: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
   },
+  persona:{
+    marginBottom:"10%",
+    height:70,
+    width:70,
+    backgroundColor:"#d5dbdb",
+    borderRadius:50,
+
+  },
+  button: {
+    flex: 1,
+    padding: 15,
+    margin: 5,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  buttonLogin: {
+    backgroundColor: '#4CAF50', 
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  
+  buttonRegister: {
+    backgroundColor: '#4CAF50',
+    paddingVertical: 15, // Asegura que el botón tenga un tamaño adecuado
+    paddingHorizontal: 25,
+    borderRadius: 8,
+    alignItems: 'center',
+  },
+  
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  container: {
+    paddingHorizontal: 25,
+    justifyContent: 'center', // Centra verticalmente
+    alignItems: 'center',  
+    width: '100%',
+    backgroundColor: '#4A90E2',
+    borderRadius: 10,
+    padding: 40,
+    elevation: 5,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: 20,
+  },
+  input: {
+    width: '100%',        // Ocupa todo el ancho disponible
+    paddingVertical: 10,  // Espacio interno arriba y abajo
+    paddingHorizontal: 15,// Espacio interno a los lados
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
+    marginBottom: 15,     // Espacio entre cada input
+    backgroundColor: '#fff', // Fondo blanco (opcional)
+  },
+  logo: {
+    marginTop:"20%",
+    marginBottom:"0%",
+    width: 240,
+    height: 240,
+    
+}
 });
 
 export default LoginScreen;
